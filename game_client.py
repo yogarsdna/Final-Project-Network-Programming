e0d569908abebad202a84851f6201e5e4e99e216
yogarsdna <gedeyoga54@gmail.com>
2022-05-24 21:38:09 +0700

    Change position
    Change the position of the initialize variables to make it easy to read

--- game_client.py
+++ game_client.py
@@ -13,6 +13,17 @@ client = None
 HOST_ADDR = "127.0.0.1"
 HOST_PORT = 9090
 
+#Initialize variables that will be used
+your_name = ""
+opponent_name = ""
+game_round = 0
+game_timer = 4
+your_choice = ""
+opponent_choice = ""
+TOTAL_NO_OF_ROUNDS = 3
+your_score = 0
+opponent_score = 0
+
 #Top frame consisting of label name and a button widget 
 top_welcome_frame= tk.Frame(window_main)
 lbl_name = tk.Label(top_welcome_frame, text = "Name:")
@@ -31,17 +42,6 @@ lbl_welcome.pack()
 lbl_line_server = tk.Label(top_message_frame, text="***********************************************************")
 lbl_line_server.pack_forget()
 top_message_frame.pack(side=tk.TOP)
-
-#Initialize variables that will be used
-your_name = ""
-opponent_name = ""
-game_round = 0
-game_timer = 4
-your_choice = ""
-opponent_choice = ""
-TOTAL_NO_OF_ROUNDS = 3
-your_score = 0
-opponent_score = 0
 
 #Connect client's name :
 def connect():

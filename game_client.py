a95be520d4170e2e57666888ba129b910a234cdf
salmalailia <73702347+salmalailia@users.noreply.github.com>
2022-05-24 21:29:53 +0700

    game_client
    #Main Game Window

--- game_client.py
+++ game_client.py
@@ -9,6 +9,19 @@ client = None
 HOST_ADDR = "127.0.0.1"
 HOST_PORT = 9090
 
+#Main Game Window 
+window_main = tk.Tk()
+window_main.title("Game Client")
+your_name = ""
+opponent_name = ""
+game_round = 0
+game_timer = 4
+your_choice = ""
+opponent_choice = ""
+TOTAL_NO_OF_ROUNDS = 3
+your_score = 0
+opponent_score = 0
+
 #Initialize variables that will be used
 your_name = ""
 opponent_name = ""
